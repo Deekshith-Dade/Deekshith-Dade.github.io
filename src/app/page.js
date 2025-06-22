b9bdd0b5b7dbeaa99b639b94f5ccb170163e0bcb
Deekshith-Dade <deekshithreddy180@gmail.com>
2025-06-21 23:48:12 -0600

    resume

--- src/app/page.js
+++ src/app/page.js
@@ -3,7 +3,6 @@ import Navbar from "../components/Navbar";
 import Center from "@/components/Center";
 import Projects from "@/components/Projects";
 import Climax from "@/components/Climax";
-import HobbyHighlights from "@/components/HobbyHighlights";
 
 export default function Home() {
   return (
@@ -13,8 +12,6 @@ export default function Home() {
       <Center />
       <hr className="border-t-[2px] border-r-green p-4"></hr>
       <Projects />
-      <hr className="border-t-[2px] border-r-green p-4"></hr>
-      <HobbyHighlights />
       <hr className="border-t-[2px] border-r-green p-4"></hr>
       <Climax />
     </div>
